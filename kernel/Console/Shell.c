409fad649a911612e1da71bdcdc95c5c1b2f5e95
Geoffrey Lovelace <geoffrey4444@gmail.com>
2026-02-09 12:09:35 -0800

    Add help command

--- kernel/Console/Shell.c
+++ kernel/Console/Shell.c
@@ -45,7 +45,7 @@ size_t tokenize_command(char *command, char *tokens[], size_t max_tokens) {
 void dispatch_command(char *command) {
   const size_t max_tokens = MAX_SHELL_ARGS;  // max number of arguments
   char *tokens[MAX_SHELL_ARGS];
-  
+
   const size_t argc = tokenize_command(command, tokens, max_tokens);
   if (argc == 0) {
     // empty command; just return
@@ -56,7 +56,7 @@ void dispatch_command(char *command) {
   int result = 0;
 
   // Loop over available commands, and dispatch if command found
-  const char * const *cargv = (const char * const *)tokens;
+  const char *const *cargv = (const char *const *)tokens;
 
   for (size_t i = 0; i < number_of_commands; ++i) {
     if (string_compare(cargv[0], commands[i].name)) {
@@ -67,13 +67,14 @@ void dispatch_command(char *command) {
         console_print("\n");
       }
       return;
-    }    
+    }
   }
 
   // No command found
   console_print("Error: the command ");
   console_print(cargv[0]);
-  console_print(" could not be recognized. Type help for a list of commands.\n");
+  console_print(
+      " could not be recognized. Type help for a list of commands.\n");
 }
 
 void print_prompt(void) { console_print("> "); }
@@ -81,10 +82,19 @@ void print_prompt(void) { console_print("> "); }
 // Handlers for built-in commands
 
 int help_handler(size_t argc, const char *const *argv) {
+  // Arguments are unused
   (void)argc;
   (void)argv;
-  console_print("Command not yet implemented\n");
-  return 1;
+  console_print("CharcoalOS available commands:\n\n");
+  const size_t number_of_commands = sizeof(commands) / sizeof(commands[0]);
+  for (size_t i = 0; i < number_of_commands; ++i) {
+    console_print(commands[i].name);
+    console_print("\t\t");
+    console_print(commands[i].help_text);
+    console_print("\n");
+  }
+  console_print("\n");
+  return 0;
 }
 
 int info_handler(size_t argc, const char *const *argv) {
