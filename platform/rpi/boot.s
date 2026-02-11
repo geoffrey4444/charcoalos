2fcba45ac2bbbaab009deb2ee042c334bcdfee3c
Geoffrey Lovelace <geoffrey4444@gmail.com>
2026-02-10 19:57:31 -0800

    Clean up rpi boot.s formatting

--- platform/rpi/boot.s
+++ platform/rpi/boot.s
@@ -14,12 +14,12 @@ _start:
 	// [23:16] = Aff2
 	// [31:24] = Aff3
 	// For pi, Aff0 = the core id (0, 1, 2, or 3)
-        mrs x7, mpidr_el1   // mrs move from system register to general register
-	and x7, x7, #3      // zero all but the lowest 2 bits (enough to count up to 3)
-	cbz x7, 0f          // compare and branch to label 0 forward if zero
-1:	wfe                 // wait for event: don't spin and drain power to wait, wait until something happens
-	b  1b               // if event never comes, it sleeps forever, but on event, it just goes back to sleep after wake
-	                    // "polite" way to park a core
+   mrs x7, mpidr_el1   // mrs move from system register to general register
+	 and x7, x7, #3      // zero all but the lowest 2 bits (enough to count up to 3)
+	 cbz x7, 0f          // compare and branch to label 0 forward if zero
+1: wfe                 // wait for event: don't spin and drain power to wait, wait until something happens
+	 b  1b               // if event never comes, it sleeps forever, but on event, it just goes back to sleep after wake
+	                     // "polite" way to park a core
 0:	
 	
 	ldr x5, =__stack_top // load register x5; =stack_top supplied somehow?
@@ -41,7 +41,7 @@ _start:
 	bl kmain             // Branch to kmain, put return address in x30
 	                     //   kmain is a c function that will compile to
 	                     //   the symbol kmain that assembly calls
-	// b .                  // Branch to self, no return address (inf loop)
+	// b .               // Branch to self, no return address (inf loop)
 	// Nope! Politely park the core to minimize power draw
 halt:
 	wfe
