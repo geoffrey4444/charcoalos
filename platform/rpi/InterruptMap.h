c1ea1c131d7d05d594c6d404d4169a2e571b9732
Geoffrey Lovelace <geoffrey4444@gmail.com>
2026-02-15 16:29:39 -0800

    Add platform-dependent interrupt MMIO addresses

---
+++ platform/rpi/InterruptMap.h
@@ -0,0 +1,19 @@
+// Distributed under the MIT license.
+// See LICENSE.txt for details.
+
+#pragma once
+
+// Define base addresses for the interrupt controller for timer, IRQs.
+#define GICD_BASE 0x40041000
+#define GICC_BASE 0x40042000
+
+#define GICD_CTLR = (GICD_BASE + 0x000)
+#define GICD_ISENABLER0 (GICD_BASE + 0x100)
+#define GICD_IPRIORITYR (GICD_BASE + 0x400)
+#define GICD_ITARGETSR (GICD_BASE + 0x800)
+
+
+#define GICC_CTLR (GICC_BASE + 0x000)
+#define GICC_PMR (GICC_BASE + 0x004)
+#define GICC_IAR (GICC_BASE + 0x00C)
+#define GICC_EOIR (GICC_BASE + 0x010)
